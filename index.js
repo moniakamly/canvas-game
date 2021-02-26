89f5541496c810aaab7fb99b1e8f7f2eb79c2f2f
AYMEN <moniakamli95@gmail.com>
2021-02-26 14:57:30 +0100

    detect player hit

--- index.js
+++ index.js
@@ -97,8 +97,9 @@ function spawnEnemies() {
     }, 1000)
 }
 
+let animationId; 
 function animate() {
-    requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height); 
     player.draw(); 
 
@@ -108,7 +109,15 @@ function animate() {
 
     enemies.forEach((enemy, enemyIndex) => {
         enemy.update();
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y); 
 
+        // End game
+        // When the enemy touches the player
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId);
+        }
+
+        // Destroy enemy whenever we hit it 
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y); 
             // When the enemy and the projectile touch 
